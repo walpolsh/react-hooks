2f9eb4eecbdc3f20880fa2205c89df68a7a1124f
Paul Walsh <twentytwentyk@live.com>
2018-11-15 15:52:13 -0500

    first

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import './App.css'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+function Todo({ todo, index }) {
+  return(
+    <div className='todo'>
+      {todo.text}
+    </div>
+  )
+}
+
+function TodoForm({addTodo}) {
+  const [value, setValue] = useState('');
+  const handleSubmit = e => {
+    e.preventDefault();
+    if(!value) return;
+    addTodo(value)
+    setValue('');
   }
+  return(
+    <form onSubmit={handleSubmit}>
+      <input type='text' className='input' value={value} placeholder='add todo' onChange={e => setValue(e.target.value)}></input>
+    </form>
+  )
+}
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      text: 'Learn about hooks',
+      isCompleted: false,
+    },
+    {
+      text: 'Go to Henry\'s',
+      isCompleted: false,
+    },
+    {
+      text: 'Eat a big bowl of tuna w/veggies and ramen',
+      isCompleted: false,
+    }
+  ])
+
+  const addTodo = text => {
+    const NewTodos = [...todos, {text}]
+    setTodos(NewTodos)
+  }
+  return (
+    <div className="app">
+      <div className='todo-list'>
+        {todos.map((todo, index) => (
+          <Todo key={index} index={index} todo={todo} />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  )
 }
 
 export default App;
